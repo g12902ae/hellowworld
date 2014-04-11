0487db6b1812ebb103b77d0b5c21981df40d1f51
g12902ae <g12902ae@s30406.tsuda.ac.jp>
2014-04-11 11:05:05 +0900

    新規でhellowworldを作成しました。

---
+++ src/hellowworld/Hellowworld.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hellowworld;
+
+/**
+ *
+ * @author g12902ae
+ */
+public class Hellowworld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hellow World");
+    }
+}
